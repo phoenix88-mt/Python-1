b9abf2681604b10b147141f819ef0056a753164d
phoenix88-mt <52533612+phoenix88-mt@users.noreply.github.com>
2019-07-11 17:53:30 +0300

    Update girilen 3 sayidanen büyügünü bulma.py

--- Python 1.asama kodlar/girilen 3 sayidanen büyügünü bulma.py
+++ Python 1.asama kodlar/girilen 3 sayidanen büyügünü bulma.py
@@ -2,9 +2,4 @@
 sayi1= input("lutfen bir sayi girin")
 sayi2= input("lutfen bir sayi girin")
 sayi3= input("lutfen bir sayi girin")
-eb=sayi1
-if eb<sayi2:
-    eb=sayi2
-if eb<sayi3:
-    eb=sayi3
-print (eb)
+print(max(sayi1, sayı2, sayı3))
